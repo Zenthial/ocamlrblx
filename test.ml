b380861bddccf6611ea8ebaaf8db6ab3c097074e
Thomas Schollenberger <thomas_schollenberger@vanguard.com>
2023-11-16 16:50:58 +0000

    more complex test.ml transpiling

--- test.ml
+++ test.ml
@@ -1,7 +1,7 @@
 let p s =
   let new_str = s ^ "yo" in
-  (* let other_str = "test" in *)
-  (* print_endline (new_str ^ other_str) *)
-  print_endline new_str
+  let other_str = "test" in
+  print_endline (new_str ^ other_str)
+(* print_endline new_str *)
 
 let _ = p "hello world"
